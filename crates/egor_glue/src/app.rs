b3a17f23f89bafd9584b9f0986ca5f792e4c2d60
wick3dr0se <wick3dr0se@protonmail.com>
2025-12-14 01:24:06 -0500

    refactor(app): move window title into `AppConfig`, clean up `AppHandler` & `WindowHandle` API

--- crates/egor_glue/src/app.rs
+++ crates/egor_glue/src/app.rs
@@ -1,104 +1,92 @@
 use crate::graphics::{Graphics, GraphicsInternal};
 
-use egor_app::{AppHandler, AppRunner, Window, WindowHandle, input::Input, time::FrameTimer};
+use egor_app::{
+    AppConfig, AppHandler, AppRunner, Window, WindowHandle, input::Input, time::FrameTimer,
+};
 use egor_render::Renderer;
 
+type UpdateCallback = dyn FnMut(&mut Graphics, &Input, &FrameTimer);
+
 pub struct App {
-    title: String,
-    vsync: bool,
+    update: Option<Box<UpdateCallback>>,
+    config: Option<AppConfig>,
     on_quit: Option<Box<dyn FnMut()>>,
-    hot_reload: bool,
+    vsync: bool,
 }
 
 impl App {
+    /// Create a new [`App`]
     pub fn new() -> Self {
         Self {
-            title: "Egor App".to_string(),
-            vsync: true,
+            update: None,
+            config: Some(AppConfig::default()),
             on_quit: None,
-            hot_reload: false,
+            vsync: true,
         }
     }
 
-    #[cfg(feature = "hot_reload")]
-    pub fn with_hot_reload(mut self) -> Self {
-        self.hot_reload = true;
+    /// Set application title
+    pub fn title(mut self, title: &str) -> Self {
+        if let Some(c) = self.config.as_mut() {
+            c.title = title.into();
+        }
         self
     }
 
-    pub fn title(mut self, t: impl Into<String>) -> Self {
-        self.title = t.into();
+    /// Enable or disable vsync
+    pub fn vsync(mut self, enabled: bool) -> Self {
+        self.vsync = enabled;
         self
     }
 
-    pub fn vsync(mut self, v: bool) -> Self {
-        self.vsync = v;
-        self
+    /// Run the app with a per-frame update closure
+    pub fn run(mut self, mut update: impl FnMut(&mut Graphics, &Input, &FrameTimer) + 'static) {
+        #[cfg(feature = "hot_reload")]
+        let update = {
+            dioxus_devtools::connect_subsecond();
+            move |g: &mut Graphics, i: &Input, t: &FrameTimer| {
+                dioxus_devtools::subsecond::call(|| update(g, i, t))
+            }
+        };
+        self.update = Some(Box::new(update));
+
+        let config = self.config.take().unwrap();
+        AppRunner::new(self, config).run();
     }
 
+    /// Sets a closure to call when the app is quitting
     pub fn on_quit(mut self, f: impl FnMut() + 'static) -> Self {
         self.on_quit = Some(Box::new(f));
         self
     }
+}
 
-    pub fn run<F: FnMut(&mut Graphics, &Input, &FrameTimer) + 'static>(self, update: F) {
-        // Wrap the update closure in subsecond call if hot_reload is enabled
-        let update = {
-            let mut f = update;
-            move |g: &mut Graphics, i: &Input, t: &FrameTimer| {
-                if self.hot_reload {
-                    #[cfg(feature = "hot_reload")]
-                    {
-                        use dioxus_devtools::{connect_subsecond, subsecond};
-                        connect_subsecond();
-                        subsecond::call(|| f(g, i, t))
-                    }
-                } else {
-                    f(g, i, t)
-                }
-            }
-        };
+impl AppHandler<Renderer> for App {
+    async fn with_resource(&mut self, window: WindowHandle) -> Renderer {
+        let (w, h) = (window.inner_size().width, window.inner_size().height);
+        Renderer::new(w, h, window.to_owned()).await
+    }
 
-        struct Handler<F> {
-            update: F,
-            vsync: bool,
-            on_quit: Option<Box<dyn FnMut()>>,
+    fn on_ready(&mut self, _window: &Window, r: &mut Renderer) {
+        r.set_vsync(self.vsync)
+    }
+
+    fn frame(&mut self, r: &mut Renderer, i: &Input, t: &FrameTimer) {
+        if let Some(update) = &mut self.update {
+            let mut g = Graphics::new(r);
+            update(&mut g, i, t);
+            let geometry = g.flush();
+            r.render_frame(geometry);
         }
+    }
 
-        impl<F: FnMut(&mut Graphics, &Input, &FrameTimer)> AppHandler<Renderer> for Handler<F> {
-            async fn with_resource(&mut self, window: WindowHandle) -> Renderer {
-                let (w, h) = (window.inner_size().width, window.inner_size().height);
-                Renderer::new(w, h, window.inner()).await
-            }
+    fn resize(&mut self, w: u32, h: u32, r: &mut Renderer) {
+        r.resize(w, h)
+    }
 
-            fn on_ready(&mut self, _window: &Window, r: &mut Renderer) {
-                r.set_vsync(self.vsync);
-            }
-
-            fn frame(&mut self, r: &mut Renderer, i: &Input, t: &FrameTimer) {
-                let mut g = Graphics::new(r);
-                (self.update)(&mut g, i, t);
-                let geometry = g.flush();
-                r.render_frame(geometry);
-            }
-
-            fn resize(&mut self, w: u32, h: u32, r: &mut Renderer) {
-                r.resize(w, h)
-            }
-
-            fn on_quit(&mut self) {
-                if let Some(quit) = &mut self.on_quit {
-                    quit();
-                }
-            }
+    fn on_quit(&mut self) {
+        if let Some(f) = &mut self.on_quit {
+            f();
         }
-
-        AppRunner::new(Handler {
-            update,
-            vsync: self.vsync,
-            on_quit: self.on_quit,
-        })
-        .title(self.title)
-        .run();
     }
 }
