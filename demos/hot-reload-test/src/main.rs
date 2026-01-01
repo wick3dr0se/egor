0703c1eb0a380436a7e5f1cf83abf2f453e6f6d6
Srayan Jana <valorzard.kiko@outlook.com>
2026-01-01 12:52:36 -0800

    very simple hot reload demo

---
+++ demos/hot-reload-test/src/main.rs
@@ -0,0 +1,17 @@
+use egor::{
+    app::App,
+    math::{Vec2, vec2},
+    render::Color,
+};
+
+fn main() {
+    App::new()
+        .title("Hot Reload Demo")
+        .run(move |gfx, _, _| {
+            // Feel free to change this code and see hot-reload in action!
+            gfx.rect()
+                    .at(vec2(0., 0.))
+                    .size(Vec2::splat(100.0))
+                    .color(Color::RED);
+        });
+}
